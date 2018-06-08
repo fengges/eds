5006653cc1efa8d5665717e5e2764a2e2e6d9d0b
fengges <1059387928@qq.com>
2018-06-08 10:51:09 +0800

    1

--- eds/config.py
+++ eds/config.py
@@ -1,7 +1,7 @@
 
 
 #----老师头像-----
-pic_url="e:\\"
+pic_url="C://data/fengge/"
 #----ueditor 文件地址----
 ueditor_url="F://eds"
 
--- runserver.py
+++ runserver.py
@@ -3,7 +3,7 @@ import sys,os
 
 from eds import app
 
-app.run(host="0.0.0.0",debug=True)
+app.run(host="0.0.0.0",debug=True,port=8080)
 
 #-----session_key-----
 # app.SECRET_KEY = os.urandom(24)
