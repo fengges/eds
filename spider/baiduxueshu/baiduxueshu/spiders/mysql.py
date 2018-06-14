2227876fa3611a7ea9d7a4998618b444f136fc1b
ischenrui <ischenrui@qq.com>
2018-06-14 13:39:17 +0800

    spider

---
+++ spider/baiduxueshu/baiduxueshu/spiders/mysql.py
@@ -0,0 +1,109 @@
+import pymysql.cursors
+from spider.baiduxueshu.baiduxueshu.settings import DB_SETTING
+from spider.baiduxueshu.baiduxueshu.settings import CRAWL_SETTING
+import random
+import uuid
+# 连接数据库
+class LocalDB(object):
+    connect = pymysql.Connect(
+        host=DB_SETTING['host'],
+        port=DB_SETTING['port'],
+        user=DB_SETTING['user'],
+        passwd=DB_SETTING['passwd'],
+        db=DB_SETTING['db'],
+        charset=DB_SETTING['charset']
+)
+    #---获取游标---
+    cursor = connect.cursor(cursor=pymysql.cursors.DictCursor)
+
+    # 插入论文
+    def InsertPaper(self, item):
+        sql = "INSERT INTO paper VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+
+        params = (str(uuid.uuid1()),
+            item['name'], item['url'], item['abstract'], item['org'], item['year'], item['cited_num'],
+            item['source'],
+            item['source_url'], item['keyword'], item['author'], item['author_id'], item['cited_url'],
+            item['reference_url'], item['paper_md5'])
+        self.cursor.execute(sql, params)
+        self.connect.commit()
+
+class AliyunDB(object):
+    connect = pymysql.Connect(
+        host='120.78.201.159',
+        port=3306,
+        user='root',
+        password='zdf.0126',
+        db='eds',
+        charset='utf8'
+)
+#获取游标
+    cursor = connect.cursor(cursor=pymysql.cursors.DictCursor)
+
+    # #---从search表中获取一条教师信息---
+    # def getAuthor(self):
+    #     randint = str(random.randint(1, 10))
+    #     sql = "SELECT * FROM paper_search_list WHERE search=0 AND searching=0 LIMIT " + randint + ",1;"
+    #     self.cursor.execute(sql)
+    #     teacher = self.cursor.fetchone()
+    #
+    #     # ---在查询后更改状态为：读取中---
+    #     sql = "UPDATE paper_search_list SET searching=1 where id=%s"
+    #     params = (teacher['id'])
+    #     self.cursor.execute(sql, params)
+    #     self.connect.commit()
+    #
+    #     return teacher
+
+    #---获取所有教师---
+    def getAuthor(self):
+        sql = "SELECT * FROM paper_search_list WHERE search=0 AND searching=0 and id%"+CRAWL_SETTING['total']+"="+CRAWL_SETTING['num']
+        self.cursor.execute(sql)
+        teacher = self.cursor.fetchall()
+        return teacher
+
+    # ---搜索后更新状态---
+    def UpdateAuthor(self, id):
+        sql = "UPDATE paper_search_list SET search=1 where id=%s"
+        params = (id)
+        self.cursor.execute(sql, params)
+        self.connect.commit()
+
+class TestDB(object):
+    connect = pymysql.Connect(
+        host='localhost',
+        port=3306,
+        user='root',
+        password='Cr648546845',
+        db='edstest',
+        charset='utf8'
+)
+#获取游标
+    cursor = connect.cursor(cursor=pymysql.cursors.DictCursor)
+
+    #---从search表中获取一条教师信息---
+    def getAuthor(self):
+        randint = str(random.randint(1, 10))
+        sql = "SELECT * FROM paper_search_list WHERE search=0 AND searching=0 and id%3"+CRAWL_SETTING['total']+"="+CRAWL_SETTING['num']
+        self.cursor.execute(sql)
+        teacher = self.cursor.fetchall()
+        return teacher
+
+    # 插入论文
+    def InsertPaper(self, item):
+        sql = "INSERT INTO paper VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+
+        params = (str(uuid.uuid1()),
+                  item['name'], item['url'], item['abstract'], item['org'], item['year'], item['cited_num'],
+                  item['source'],
+                  item['source_url'], item['keyword'], item['author'], item['author_id'], item['cited_url'],
+                  item['reference_url'], item['paper_md5'])
+        self.cursor.execute(sql, params)
+        self.connect.commit()
+
+    # ---搜索后更新状态---
+    def UpdateAuthor(self, id):
+        sql = "UPDATE paper_search_list SET search=1 where id=%s"
+        params = (id)
+        self.cursor.execute(sql, params)
+        self.connect.commit()
---
+++ spider/baiduxueshu/baiduxueshu/spiders/test.py
@@ -0,0 +1,52 @@
+# import random
+# randint = random.randint(1,10)
+# print(randint)
+
+# from baiduxueshu.spiders import mysql
+# db = mysql.LocalDB()
+#
+# a = db.getAuthor()
+# print('a',a)
+
+# import re
+# url = 'http://xueshu.baidu.com/s?wd=%22%E6%9D%8E%E5%81%A5%22%20%22%E5%8C%97%E4%BA%AC%E5%A4%A7%E5%AD%A6%22%20author%3A%28%E6%9D%8E%E5%81%A5%29&pn=5550&tn=SE_baiduxueshu_c1gjeupa&ie=utf-8&sc_f_para=sc_tasktype%3D%7BfirstSimpleSearch%7D&sc_hit=1&rsv_page=1'
+# page = re.findall(r"pn=([0-9]+)", url)[0]
+# print(page)
+
+# dic = {
+#    '1':'知网',
+#    '2':'维普',
+#    '3':'万方'
+# }
+#
+# a = dic.setdefault('4',None)
+# print(a)
+
+# import time
+# time.sleep(3)
+# print('kljkl')
+#
+# import requests
+#
+# url = 'http://47.106.180.108:8081/Index-generate_api_url.html?packid=7&fa=5&qty=10&port=1&format=txt&ss=1&css=&pro=&city='
+# data = requests.get(url).text
+# f1 = open('ippool.txt','w')
+# f1.write(data)
+# f1.close()
+
+# s = "课程设置是诊所法律教育的核心。实证的数据表明,在课程目标上我们有必要适当调整学生的目标定位,强调职业道德的培养。在课程结构上,值班指导部分和课堂教学部分按70:3..."
+#
+# print(s[-3:])
+
+# import uuid
+# # while 1:
+# #     print(uuid.uuid1())
+#
+# print(str(uuid.uuid1()))
+
+
+# from spider.baiduxueshu.baiduxueshu.spiders import mysql
+# db = mysql.TestDB()
+#
+# teacherlist = db.getAuthor()
+# print(teacherlist)
