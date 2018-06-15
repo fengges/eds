1bc5c2349493af093f55105e3b6905662b1a2cab
ischenrui <ischenrui@qq.com>
2018-06-15 17:52:47 +0800

    spider

--- spider/baiduxueshu/baiduxueshu/middlewares.py
+++ spider/baiduxueshu/baiduxueshu/middlewares.py
@@ -76,6 +76,14 @@ class IPPOOLS(HttpProxyMiddleware):
         url = "http://ip.11jsq.com/index.php/api/entry?method=proxyServer.generate_api_url&packid=1&fa=0&fetch_key=&qty=1&time=1&pro=&city=&port=1&format=txt&ss=1&css=&dt=1&specialTxt=3&specialJson="
         data = requests.get(url).text
         iplist = data.split('\r\n')
+
+        if iplist[0].find('当前用户可用的有效IP数量不够'):
+            time.sleep(2)
+            print('时间ip不够用')
+            url = "http://ip.11jsq.com/index.php/api/entry?method=proxyServer.generate_api_url&packid=0&fa=0&fetch_key=&qty=1&time=1&pro=&city=&port=1&format=txt&ss=1&css=&dt=1&specialTxt=3&specialJson="
+            data = requests.get(url).text
+            iplist = data.split('\r\n')
+
         result = []
         for ip in iplist:
             result.append({'ip': ip, 'success': 0, 'failure': 0})
