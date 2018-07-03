7c6f4578809dabb08de6a81b1afba7969a8f5404
ischenrui <ischenrui@qq.com>
2018-07-03 22:31:07 +0800

    spider

--- spider/paperspider/papers/papers/services/paperservices.py
+++ spider/paperspider/papers/papers/services/paperservices.py
@@ -1,22 +1,22 @@
-from papers.dbutils import dbs
+from spider.paperspider.papers.papers.dbutils import dbs
 
 
 class PaperService:
     def get_search_list(self):
-        sql = "select * from teacher_searchlist_copy where searching=0 and searched=0"
+        sql = "select * from teacher_searchlist_copy_copy where searched=0 or searched=1"
         info_list = dbs.getDics(sql)
         return info_list
 
     def update_searched(self, params):
-        sql = "update teacher_searchlist_copy set searched=%s where id=%s"
+        sql = "update teacher_searchlist_copy_copy set searched=%s where id=%s"
         dbs.exe_sql(sql, params)
 
     def update_searching(self, params):
-        sql = "update teacher_searchlist_copy set searching = searching + 1 where id=%s"
+        sql = "update teacher_searchlist_copy_copy set searching = searching + 1 where id=%s"
         dbs.exe_sql(sql, params)
 
     def update_error(self, params):
-        sql = "update teacher_searchlist_copy set error=1 where id=%s"
+        sql = "update teacher_searchlist_copy_copy set error=1 where id=%s"
         dbs.exe_sql(sql, params)
 
 
