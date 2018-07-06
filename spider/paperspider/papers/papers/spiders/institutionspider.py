7e4fa8eac12ac29e123c673fde9903b121b7af17
KiwiPPPP <jokerliwei@163.com>
2018-07-06 14:18:58 +0800

    新institution

--- spider/paperspider/papers/papers/spiders/institutionspider.py
+++ spider/paperspider/papers/papers/spiders/institutionspider.py
@@ -2,7 +2,6 @@
 import scrapy
 import os
 import re
-import time
 from spider.paperspider.papers.papers.services.paperservices import paper_service
 from spider.paperspider.papers.papers.spiders.super_spider import SuperSpider
 from spider.paperspider.papers.papers.items import *
@@ -14,29 +13,26 @@ class InstitutionSpider(SuperSpider):
     name = 'institutionspider'
     allowed_domains = []
     start_urls = ['http://xueshu.baidu.com/']
-    source_list = ["维普", "知网", "万方", "Elsevier"]
+
+    begin = 100
+    end = 500000
+    step = 20
+    count = 0
+    num = 0
 
     def parse(self, response):
-        begin = 100
-        step = 20
-        end = 500000
-        while begin + step <= end:
+        print(self.begin, self.begin + self.step)
+        search_list = self.get_search_list()
+        for s in search_list:
+            paper = UpdateInstitutionItem()
+            paper["_id"] = s["_id"]
+            paper["author"] = s["author"]
+            request_url = s["url"]
+            yield scrapy.Request(url=request_url,
+                                 meta={"paper": paper},
+                                 callback=self.parse_institution)
 
-            print(begin, begin + step)
-            search_list = paper_service.institution_search_list_from_paper(begin, begin + step)
-
-            for s in search_list:
-                paper = UpdateInstitutionItem()
-                paper["_id"] = s["_id"]
-                paper["author"] = s["author"]
-                request_url = s["url"]
-                yield scrapy.Request(url=request_url,
-                                     meta={"paper": paper},
-                                     callback=self.parse_institution)
-            begin += step
-        pass
-
-    # 解析百度学术论文详情页
+    # 解析百度学术论文详情页, 获取作者与机构
     def parse_institution(self, response):
         paper = response.meta.get("paper")
         paper_author_list = response.css(".author_wr .author_text a")
@@ -46,6 +42,31 @@ class InstitutionSpider(SuperSpider):
         if paper["author"] != author and len(l) > 0:
             paper["author"] = author
             yield paper
-        else:
+        try:
             paper_service.update_paper_search_list(paper["_id"])
-        pass
+            pass
+        except Exception as e:
+            print(e)
+            print("更新searched错误……")
+        self.count += 1
+        if self.count == self.num:
+            print(self.begin, self.begin + self.step)
+            search_list = self.get_search_list()
+
+            for s in search_list:
+                paper = UpdateInstitutionItem()
+                paper["_id"] = s["_id"]
+                paper["author"] = s["author"]
+                request_url = s["url"]
+                yield scrapy.Request(url=request_url,
+                                     meta={"paper": paper},
+                                     callback=self.parse_institution)
+
+    def get_search_list(self):
+        search_list = []
+        while len(search_list) == 0 and self.begin + self.step <= 500000:
+            search_list = paper_service.institution_search_list_from_paper(self.begin, self.begin + self.step)
+            self.begin += self.step
+        self.num = len(search_list)
+        self.count = 0
+        return search_list
