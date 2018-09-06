e2445aa4c35524ecb8947ac3e79a790415a64bf4
KiwiPPPP <jokerliwei@163.com>
2018-09-06 12:34:09 +0800

    liwei

--- eds/static/js/search/search_more.js
+++ eds/static/js/search/search_more.js
@@ -1,4 +1,7 @@
 $(function() {
+
+    //点击加载更多
+
     var toggles = $(".search-toggle");
     var contents = $(".search-hidden-content");
 
@@ -8,8 +11,19 @@ $(function() {
 
     function clickHandler(event) {
         var i= event.data.index;
-        $(this).text(contents.eq(i).is(":hidden") ? "收起信息" : "更多信息");
+        $(this).html(contents.eq(i).is(":hidden") ? "收起信息<i class='glyphicon glyphicon-chevron-up'></i>" : "更多信息<i class='glyphicon glyphicon-chevron-down'></i>");
+
         contents.eq(i).slideToggle();
+    }
+
+    //按钮“更多信息”隐藏
+    var persons = $(".search-person>li");
+    for(var i=0; i<persons.length; i++){
+        persons.eq(i).hover(function(){
+            $(this).find(".search-toggle").css('visibility', 'visible')
+        }, function(){
+            $(this).find(".search-toggle").css('visibility', 'hidden')
+        });
     }
 
 });
